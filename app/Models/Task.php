1a0a9ff37b9953988528c50a7cf8438bc1bc6dc8
Athul Suresh <athulsuresh198@gmail.com>
2024-03-05 14:54:44 +0530

    first commit

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+
+    public function team():BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function label():BelongsTo
+    {
+        return $this->belongsTo(Label::class);
+    }
+
+}
